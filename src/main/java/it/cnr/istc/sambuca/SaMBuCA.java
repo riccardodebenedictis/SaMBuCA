c209d11f407557c1f73d867f010b7ca0b5483932
riccardodebenedictis <riccardo.debenedictis@istc.cnr.it>
2018-06-19 15:24:14 +0200

    added initial state encoding..

--- src/main/java/it/cnr/istc/sambuca/SaMBuCA.java
+++ src/main/java/it/cnr/istc/sambuca/SaMBuCA.java
@@ -1,10 +1,17 @@
 package it.cnr.istc.sambuca;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import it.cnr.istc.neon.Network;
 import it.cnr.istc.neon.activation.Sigmoid;
 import it.cnr.istc.neon.error.CrossEntropy;
+import it.cnr.istc.sambuca.parser.Constant;
 import it.cnr.istc.sambuca.parser.PDDLLanguageParser;
 import it.cnr.istc.sambuca.parser.Predicate;
 import it.cnr.istc.sambuca.parser.ProblemInstance;
@@ -16,24 +23,40 @@ import it.cnr.istc.sambuca.parser.Variable;
  */
 public class SaMBuCA {
 
-    private final Network nn;
     private final ProblemInstance pi;
+    private final Network nn;
 
     public SaMBuCA(String domain_path, String problem_path) throws IOException {
         this.pi = PDDLLanguageParser.parse(domain_path, problem_path);
 
-        int input_size = 0;
+        // this is the list of all the ground predicates..
+        List<String> ground_preds = new ArrayList<>();
         for (Predicate p : pi.getDomain().getPredicates().values()) {
-            int c_size = 1;
-            for (Variable v : p.getVariables()) {
-                c_size *= v.getType().getInstances().size();
+            List<Variable> vars = p.getVariables();
+            if (vars.isEmpty())
+                ground_preds.add("(" + p.getName() + ")");
+            else {
+                Constant[][] cnsts = new Constant[vars.size()][];
+                for (int i = 0; i < vars.size(); i++) {
+                    cnsts[i] = vars.get(i).getType().getInstances()
+                            .toArray(new Constant[vars.get(i).getType().getInstances().size()]);
+                }
+                for (Constant[] cs : new CartesianProductGenerator<>(cnsts)) {
+                    ground_preds.add("(" + p.getName() + " "
+                            + Stream.of(cs).map(argument -> argument.toString()).collect(Collectors.joining(" "))
+                            + ")");
+                }
             }
-            input_size += c_size;
         }
 
+        // this is the initial state..
+        Set<String> init = new HashSet<>();
         for (Term t : pi.getProblem().getInitEls()) {
-            System.out.println(t);
+            init.add(t.toString());
         }
-        this.nn = new Network(new CrossEntropy(), new Sigmoid(), input_size, 8, 8, 1);
+
+        // we create the neural network..
+        this.nn = new Network(new CrossEntropy(), new Sigmoid(), ground_preds.size() * 2, ground_preds.size(),
+                ground_preds.size(), 1);
     }
 }
